ec9ea61ab3cb4113431b7986fdbbd69fe9b8a765
MichaelRomero <liikemike@aim.com>
2019-05-31 17:18:55 -0400

    added handleChange to search input as well as local state storing the changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,11 +3,23 @@ import searchIcon from '../images/search.png'
 
 
 class SearchBar extends React.Component {
+
+  state = {
+    searchText: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      searchText: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="nav-search">
         <img src={searchIcon} alt=""/>
         <input
+          onChange={this.handleChange}
           className="search"
           placeholder="Search"
         />
