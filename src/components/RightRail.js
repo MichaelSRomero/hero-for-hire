aa86adbef63926000b66afb70fcd2f8484bdc659
MichaelRomero <liikemike@aim.com>
2019-06-04 14:01:26 -0400

    added styling to list items

--- src/components/RightRail.js
+++ src/components/RightRail.js
@@ -9,6 +9,19 @@ class RightRail extends React.Component {
 
         <footer>
           <div className="footer-links">
+            <div className="row">
+              <li>About</li>
+              <li>Help Center</li>
+              <li>Privacy & Terms</li>
+            </div>
+            <div className="row">
+              <li>Advertising</li>
+              <li>Business Services</li>
+            </div>
+            <div className="row">
+              <li>Get the LinkedIn app</li>
+              <li>More</li>
+            </div>
           </div>
 
           <div className="footer-copyright">
