4a2f1fc73cfe05a72fec106e768c8156d4cf435e
MichaelRomero <liikemike@aim.com>
2019-06-04 14:40:22 -0400

    added arrow icons inside footer-link

--- src/components/RightRail.js
+++ src/components/RightRail.js
@@ -4,7 +4,19 @@ import linkArrow from '../images/link-arrow.png'
 import linkArrowHover from '../images/link-arrow-hover.png'
 
 class RightRail extends React.Component {
+  state = {
+    currentHover: ''
+  }
+
+  onHoverChange = (item) => {
+    this.setState({
+      currentHover: item
+    });
+  }
+
   render() {
+    const { currentHover } = this.state
+
     return (
       <div className="right-rail">
         <PromotionBanner />
@@ -14,15 +26,28 @@ class RightRail extends React.Component {
             <div className="row">
               <li>About</li>
               <li>Help Center</li>
-              <li>
+              <li
+                onMouseEnter={() => this.onHoverChange('privacy')}
+                onMouseLeave={() => this.onHoverChange('')}>
                 Privacy & Terms
-                <img src={linkArrow} alt="click me"/>
+                <img
+                  src={currentHover === 'privacy' ? linkArrowHover : linkArrow}
+                  alt="click me"/>
               </li>
             </div>
+
             <div className="row">
               <li>Advertising</li>
-              <li>Business Services</li>
+              <li
+                onMouseEnter={() => this.onHoverChange('business')}
+                onMouseLeave={() => this.onHoverChange('')}>
+                Business Services
+                <img
+                  src={currentHover === 'business' ? linkArrowHover : linkArrow}
+                  alt="click me"/>
+              </li>
             </div>
+
             <div className="row">
               <li>Get the LinkedIn app</li>
               <li>More</li>
