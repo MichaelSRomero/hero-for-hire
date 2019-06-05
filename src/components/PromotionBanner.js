74eaa10669f6839e89c16d62ef00aaf43591acac
MichaelRomero <liikemike@aim.com>
2019-06-05 17:19:18 -0400

    finished promotioncards

--- src/components/PromotionBanner.js
+++ src/components/PromotionBanner.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import PromotionCard from './PromotionCard'
 
+const promos = [
+  {
+    icon: "https://c.wallhere.com/photos/5e/b9/1366x768_px_5_Centimeters_Per_Second_anime_city_clouds_Makoto_Shinkai_Skyscraper-578455.jpg!d",
+    header: "Companies apply to you.",
+    description: "Tell us what you love to do and have companies compete over you."
+  },
+  {
+    icon: "https://badger11.files.wordpress.com/2006/12/boxes.jpg?w=456&h=456",
+    header: "#1 Office Movers in Kanto",
+    description: "#1 Office & Commercial Movers in Kanto! Since 1983. Now 10% Off, Call Now !"
+  },
+  {
+    icon: "https://data.whicdn.com/images/302971672/large.jpg",
+    header: "MS Criminal Justice",
+    description: "Earn Your Master of Criminal Justice in Law Development in as Few as 10 Months."
+  }
+]
+
 class PromotionBanner extends React.Component {
+
+  createPromotionCards = () => {
+    return promos.map(promo => <PromotionCard promo={promo}/>)
+  }
 
   render() {
     return (
@@ -10,7 +32,7 @@ class PromotionBanner extends React.Component {
           <span>Promoted</span>
         </div>
 
-        <PromotionCard />
+        {this.createPromotionCards()}
       </div>
     )
   }
--- src/components/PromotionCard.js
+++ src/components/PromotionCard.js
@@ -3,20 +3,25 @@ import promoArrow from '../images/promo-arrow.png'
 
 class PromotionCard extends React.Component {
   render() {
+    const {icon, header, description} = this.props.promo
+
     return (
       <div className="promotion-card">
         <img
           className="promotion-icon"
-          src="https://c.wallhere.com/photos/5e/b9/1366x768_px_5_Centimeters_Per_Second_anime_city_clouds_Makoto_Shinkai_Skyscraper-578455.jpg!d"
+          src={icon}
           alt="icon"/>
         <div className="promotion-info">
-          <h5>Companies apply to you.</h5>
+          <h5>{header}</h5>
+          <p>{description}</p>
         </div>
 
-        <img
-          className="promotion-arrow"
-          src={promoArrow}
-          alt="" />
+        <div className="outer-arrow">
+          <img
+            className="promotion-arrow"
+            src={promoArrow}
+            alt="" />
+        </div>
       </div>
     )
   }
