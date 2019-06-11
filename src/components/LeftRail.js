ea836e0521ca57a6b78465870297f7bfb7a59db9
MichaelRomero <liikemike@aim.com>
2019-06-11 18:40:44 -0400

    added hashtag bubble icon along with a div

--- src/components/LeftRail.js
+++ src/components/LeftRail.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { getCompanies } from '../adapter/companyAdapter'
 import CommunityCard from './CommunityCard';
 import speechBubbleIcon from '../images/speech-bubble.png';
+import hashtagBubbleIcon from '../images/hashtag-bubble.png';
 
 class LeftRail extends React.Component {
   componentDidMount() {
@@ -15,13 +16,22 @@ class LeftRail extends React.Component {
     return ( companies.length > 0 &&
       <div className="left-rail">
         <CommunityCard communities={[companies[0], companies[1], companies[2]]}/>
-        
+
         <div className="summary-box-2">
           <img src={speechBubbleIcon}/>
 
           <div className="summary-box-text">
             <h3>No groups yet</h3>
             <p>Once you join groups, you will see them here</p>
+          </div>
+        </div>
+
+        <div className="summary-box-3">
+          <img src={hashtagBubbleIcon}/>
+
+          <div className="summary-box-text">
+            <h3>No hashtags yet</h3>
+            <p>Once you follow hashtags, you will see them here</p>
           </div>
         </div>
       </div>
