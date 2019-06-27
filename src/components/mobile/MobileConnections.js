8008326a2d98f81c0772bb472c01bfa6999e5c8f
MichaelRomero <liikemike@aim.com>
2019-06-27 19:16:19 -0400

    added connection boxes for mobile

--- src/components/mobile/MobileConnections.js
+++ src/components/mobile/MobileConnections.js
@@ -4,6 +4,14 @@ class MobileConnections extends React.Component {
   render() {
     return (
       <div className="connections-mobile">
+        <div className="connection-box">
+          <span>250</span> Connections
+        </div>
+
+        <div className="connection-box">
+          <span>+</span> Find connections
+        </div>
+        
       </div>
     )
   }
