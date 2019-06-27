8df4e2de07fffae76296f31104cef587ee554046
MichaelRomero <liikemike@aim.com>
2019-06-26 23:52:51 -0400

    changed style for mobile version of app

--- src/components/NavBarMobile.js
+++ src/components/NavBarMobile.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import UserIcon from './UserIcon';
+import SearchBar from './SearchBar';
 
 class NavBarMobile extends React.Component {
   render() {
     return (
       <div className="nav-bar-mobile">
         <UserIcon />
+        <SearchBar />
       </div>
     )
   }
