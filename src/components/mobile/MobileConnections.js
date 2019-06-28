60762b41cd930c8011424d07af6023cd8b694f63
MichaelRomero <liikemike@aim.com>
2019-06-27 20:12:11 -0400

    added mobile invitations div

--- src/components/mobile/MobileConnections.js
+++ src/components/mobile/MobileConnections.js
@@ -3,15 +3,21 @@ import React from 'react';
 class MobileConnections extends React.Component {
   render() {
     return (
-      <div className="connections-mobile">
-        <div className="connection-box">
-          <span className="connection-header">250</span> Connections
+      <div className="connection-toolbar">
+        <div className="connections-mobile">
+          <div className="connection-box">
+            <span className="connection-header">250</span> Connections
+          </div>
+
+          <div className="connection-box">
+            <span className="connection-header">+</span> Find connections
+          </div>
+
         </div>
 
-        <div className="connection-box">
-          <span className="connection-header">+</span> Find connections
+        <div className="mobile-invitations">
+          <span>No pending invitations</span>
         </div>
-
       </div>
     )
   }
