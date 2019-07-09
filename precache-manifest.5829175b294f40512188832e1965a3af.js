78affe2b5b6b8afc08c70319648ce02f35494af2
MichaelRomero <liikemike@aim.com>
2019-07-09 17:36:23 -0400

    Updates

---
+++ precache-manifest.5829175b294f40512188832e1965a3af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d002976925a512b5c3a3",
+    "url": "/hero-for-hire/static/css/main.b4e7f677.chunk.css"
+  },
+  {
+    "revision": "d002976925a512b5c3a3",
+    "url": "/hero-for-hire/static/js/main.904f517c.chunk.js"
+  },
+  {
+    "revision": "349d0e4cf37ad6603281",
+    "url": "/hero-for-hire/static/js/runtime~main.0778e7d1.js"
+  },
+  {
+    "revision": "ad837c7ab264d8990289",
+    "url": "/hero-for-hire/static/js/2.609bf0ca.chunk.js"
+  },
+  {
+    "revision": "3d940ff00a6f60437aa696b353760723",
+    "url": "/hero-for-hire/static/media/speech-bubble.3d940ff0.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hero-for-hire/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b4ee8206d836a2ecb2dea4145a9dca36",
+    "url": "/hero-for-hire/index.html"
+  }
+];
